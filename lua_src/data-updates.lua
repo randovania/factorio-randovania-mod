24de0f0f18d5c961475fe78faf3f4098bd901a48
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2024-03-10 15:07:34 +0200

    Start python package

--- lua_src/data-updates.lua
+++ lua_src/data-updates.lua
@@ -1,23 +1,28 @@
-
 for name, tech in pairs(data.raw["technology"]) do
     tech.hidden = true
 end
 
 function add_randovania_tech(param)
-    data:extend(
-        {
-            {
-                type = "technology",
-                name = param.name,
-                icon_size = 256,
-                icon_mipmaps = 4,
-                icon = param.icon,
-                effects = {},
-                unit = param.costs,
-                order = param.order or param.name,
-                prerequisites = param.prerequisites,
-            },
-        })
+    local prototype = {
+        type = "technology",
+        name = param.name,
+        icon_size = 256,
+        icon_mipmaps = 4,
+        icon = param.icon,
+        effects = {},
+        unit = param.costs,
+        order = param.order or param.name,
+        prerequisites = param.prerequisites,
+    }
+    if param.replicate then
+        local replicated = data.raw["technology"][param.replicate]
+        prototype.effects = replicated.effects
+        prototype.localised_name = replicated.localised_name or {"technology-name." .. param.replicate}
+        prototype.localised_description = replicated.localised_description or {"technology-description." .. param.replicate}
+        replicated.effects = {}
+        replicated.enabled = false
+    end
+    data:extend { prototype }
 end
 
 local tech_tree = require("generated.tech-tree")
