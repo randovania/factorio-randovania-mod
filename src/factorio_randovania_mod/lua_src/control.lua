f7b7f4ec01fbad368b393e15eee98cb545d42850
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2024-03-20 20:33:15 +0200

    Persist freebies given, retry if inventory is full and new joiners

--- src/factorio_randovania_mod/lua_src/control.lua
+++ src/factorio_randovania_mod/lua_src/control.lua
@@ -1,32 +1,46 @@
-
 local LOCAL_UNLOCKS = require("generated.local-unlocks")
 local STARTING_TECH = require("generated.starting-tech")
 
-local function add_freebie(force, name)
-    local stack = {
-        name = name,
-        count = game.item_prototypes[name].stack_size,
-    }
+local playersAlreadyWarned = {}
 
-    for _, player in pairs(force.players) do
-        if player.valid and player.character and player.character.valid then
-            local character = player.character
+---@param player_index int
+local function give_pending_freebies(player_index)
+    local freebies = global.player_pending_freebies[player_index]
+    local player = game.get_player(player_index)
+    if not player or not freebies then
+        return
+    end
+
+    local character = player.character
+    if player.valid and character and character.valid then
+        while freebies[1] do
+            local stack = freebies[1]
             if character.can_insert(stack) then
                 local sent = character.insert(stack)
-                player.print(string.format("Received %dx [item=%s]", sent, name))
+                player.print(string.format("Received %dx [item=%s]", sent, stack.name))
+                playersAlreadyWarned[player_index] = nil
+                table.remove(freebies, 1)
             else
-                player.print("Unable to provide freebies, inventory is full.")
+                if not playersAlreadyWarned[player_index] then
+                    player.print("Unable to provide freebies, inventory is full.")
+                    playersAlreadyWarned[player_index] = true
+                end
+                return
             end
         end
     end
 end
 
+---@param force LuaForce
+---@param name string
 local function unlock_tech(force, name)
     local research = force.technologies[name]
-    research.force.print(string.format("Unlocking %s", name))
+    -- research.force.print(string.format("Unlocking %s", name))
     research.researched = true
 end
 
+---@param force LuaForce
+---@param progressive table
 local function progressive_unlock(force, progressive)
     for _, name in ipairs(progressive) do
         if not force.technologies[name].researched then
@@ -35,27 +49,50 @@ local function progressive_unlock(force, progressive)
     end
 end
 
-local function give_freebies(research)
-    -- force.print(string.format("Giving freebies %s", research.name))
+---@param research LuaTechnology
+local function freebies_for_tech(research)
+    local result = {}
 
     for _, effect in ipairs(research.effects or {}) do
         if effect.type == "unlock-recipe" then
-            -- force.print(string.format("Recipe unlocked: %s", effect.recipe))
             local recipe_proto = game.recipe_prototypes[effect.recipe]
             for _, product in ipairs(recipe_proto.products) do
                 local amount = (product.amount or product.amount_max or 0) - (product.catalyst_amount or 0)
                 if product.type == "item" and amount > 0 then
-                    add_freebie(research.force, product.name)
+                    table.insert(result, {
+                        name = product.name,
+                        count = game.item_prototypes[product.name].stack_size,
+                    })
                 end
             end
         end
+    end
+
+    return result
+end
+
+---@param research LuaTechnology
+local function give_freebies(research)
+    -- force.print(string.format("Giving freebies %s", research.name))
+
+    local freebies = freebies_for_tech(research)
+
+    for _, freebie in pairs(freebies) do
+        table.insert(global.total_freebies, freebie)
+    end
+
+    for _, player in pairs(research.force.players) do
+        for _, freebie in pairs(freebies) do
+            table.insert(global.player_pending_freebies[player.index], freebie)
+        end
+        give_pending_freebies(player.index)
     end
 end
 
 ---@param event EventData.on_research_finished Event data
 local function on_research_finished(event)
     local research = event.research
-    research.force.print(string.format("Researched %s", research.name))
+    -- research.force.print(string.format("Researched %s", research.name))
 
     give_freebies(research)
 
@@ -66,11 +103,42 @@ end
 
 script.on_event(defines.events.on_research_finished, on_research_finished)
 
+---@param event EventData.on_player_main_inventory_changed
+local function on_player_main_inventory_changed(event)
+    give_pending_freebies(event.player_index)
+end
+
+script.on_event(defines.events.on_player_main_inventory_changed, on_player_main_inventory_changed)
+
+---@param event EventData.on_player_joined_game
+local function on_player_joined_game(event)
+    -- We shouldn't need this.
+    -- if not global.player_pending_freebies then
+    --     global.player_pending_freebies = {}
+    --     global.total_freebies = {}
+    -- end
+
+    if not global.player_pending_freebies[event.player_index] then
+        -- New joiner!
+        local freebies = {}
+        for _, freebie in pairs(global.total_freebies) do
+            table.insert(freebies, freebie)
+        end
+        global.player_pending_freebies[event.player_index] = freebies
+    end
+    give_pending_freebies(event.player_index)
+end
+
+script.on_event(defines.events.on_player_joined_game, on_player_joined_game)
+
 script.on_init(function()
+    global.player_pending_freebies = {}
+    global.total_freebies = {}
+
     local player_tech = game.forces.player.technologies
     for _, tech in ipairs(STARTING_TECH) do
-        -- TODO: this doesn't give the freebies
         player_tech[tech].researched = true
+        give_freebies(player_tech[tech])
     end
 end)
 
