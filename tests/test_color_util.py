98a63521f4309a403e16ead996c37dbcbf3f1969
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2024-05-20 20:19:50 +0300

    Add test and CI

---
+++ tests/test_color_util.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pytest
+
+from factorio_randovania_mod import color_util
+
+
+@pytest.mark.parametrize(
+    ["rgb", "expected"],
+    [
+        ((255, 0, 0), (0, 1, 255)),
+        ((0, 255, 0), (1 / 3, 1, 255)),
+        ((0, 0, 255), (2 / 3, 1, 255)),
+        ((255, 255, 255), (0, 0, 255)),
+        ((0, 0, 0), (0, 0, 0)),
+    ],
+)
+def test_rgb_to_hsv(rgb, expected):
+    arr = np.array(rgb)
+    result = color_util.rgb_to_hsv(arr)
+    assert (result == np.array(expected)).all()
